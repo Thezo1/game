0c9eb8bb85146af9b579f72d6a64d6458ab3d5cf
Thezo1 <rasgenes@gmail.com>
2022-11-20 16:07:14 +0100

    second iteration

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ mod platform;
 
 fn main()
 {
-    PlatformState::platform_startup("test", 0, 0, 100, 100);
+    let mut platform = PlatformState::new();
+    if platform.platform_startup("test", 0, 0, 100, 100)
+    {
+        while platform.platform_pump_message()
+        {
+        }
+    }
+    platform.platform_shutdown();
 }
